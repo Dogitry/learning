6f0a28a3e1a9f9f8b1e007aecfedcafbc8a994a4
Donnie <3415389520@qq.com>
2022-11-17 10:50:43 +0800

    issue 43 change b to p

--- hello2.cpp
+++ hello2.cpp
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int main(){
-    int a,b;
-    scanf("%d %d",&a,&b);
-    printf("%d\n %d\n",a,b);
-//hotfix
+    int a,p;
+    scanf("%d %d",&a,&p);
+    printf("%d\n %d\n",a,p);
+//not hotfix
+//issue43
     return 0;
 }
